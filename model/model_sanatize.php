848da62e7806429be0f53216d075ee747baaf504
koriley <kevin@blueshorestudios.com>
2016-04-29 15:48:06 -0500

    added the email checker.

--- model/model_sanatize.php
+++ model/model_sanatize.php
@@ -1,21 +1,29 @@
 <?php
 
-
-
 /**
  * Sanatize varaibles before we send them to the database.
  *
  * @author koriley
  */
 class sanatize {
-    
-    function removeHash($hashTag){
+
+    function removeHash($hashTag) {
         $hash = str_replace('#', '', $hashTag);
         return $hash;
     }
-    
-    function removeTags($string){
+
+    function removeTags($string) {
         $newString = strip_tags($string);
         return $newString;
     }
+
+    function checkEmail($email) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $isEmail = "false";
+        } else {
+            $isEmail = "true";
+        }
+        return $isEmail;
+    }
+
 }
